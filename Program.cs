252c92916d2648f76a4568a865d21dbe8b18e644
agent-doc <doc.game@list.ru>
2023-03-22 21:13:00 +0300

    add Блок схема

--- Program.cs
+++ Program.cs
@@ -22,14 +22,14 @@ string[] homework1(string[] mass, int a)
         }
  
     }
-    int a_raz = a-m;
+    int a_raz = a - m;
     string[] rez = new string[a_raz];
     int b = 0;
-    for (int i=0;i<a;i++)
+    for (int i = 0; i < a; i++)
     {
         if (mass[i].Length <= 3)
         {
-            rez[b]= mass[i];
+            rez[b] = mass[i];
             b++;
         }
     }
@@ -40,7 +40,7 @@ int dz1_a = 0;
 Console.Write("Введите количество элементов массива: ");
 dz1_a = Convert.ToInt32(Console.ReadLine());
 string[] mass_dz1 = ruc(dz1_a);
-Console.WriteLine("Массив: [ {0}{1}",String.Join(", ",mass_dz1)," ]");
+Console.WriteLine("Массив: [ {0}{1}", String.Join(", ",mass_dz1)," ]");
 string[] rez = homework1(mass_dz1,dz1_a);
 Console.WriteLine("Колличество четных элеммента массива: ");
 Console.WriteLine("Массив: [ {0}{1}",String.Join(", ", rez)," ]");
